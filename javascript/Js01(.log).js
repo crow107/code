0aebe532a464dc28e4676b4e9ccf023925913f44
Kanishk <rawat27kanishk@gmail.com>
2022-01-28 20:03:41 +0530

    Js

---
+++ javascript/Js01(.log).js
@@ -0,0 +1,6 @@
+console.log('CodeWithHarry');  
+console.log(1); 
+console.log(true); 
+console.log(null); ;  
+console.log([1, 2, 3]); // array inside log
+console.log({name:"Harry", language:"JavaScript", tutorial:2}); // object inside log
