c155a31eb0b319c13d268891fd05dc1f791ad014
Kanishk <rawat27kanishk@gmail.com>
2022-01-28 20:53:04 +0530

    Jscommit

---
+++ javascript/Js01(.log).js
@@ -0,0 +1,7 @@
+console.log('Kanishk');  
+console.log(1); 
+console.log(true); 
+console.log(null); ;  
+console.log([1, 2, 3]); 
+console.log({name:"kanni", language:"JavaScript", tutorial:2}); 
+
