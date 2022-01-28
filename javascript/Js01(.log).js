b4c83ea6de3a8395ec61f7466ecbc20ade9f7fdc
kanishk rawat <97908874+Kanishkrawatt@users.noreply.github.com>
2022-01-28 20:08:32 +0530

    Update Js01(.log).js

--- javascript/Js01(.log).js
+++ javascript/Js01(.log).js
@@ -1,6 +1,6 @@
-console.log('CodeWithHarry');  
+console.log('Kanishk');  
 console.log(1); 
 console.log(true); 
 console.log(null); ;  
 console.log([1, 2, 3]); // array inside log
-console.log({name:"Harry", language:"JavaScript", tutorial:2}); // object inside log
+console.log({name:"kani", language:"JavaScript", tutorial:2}); // object inside log
