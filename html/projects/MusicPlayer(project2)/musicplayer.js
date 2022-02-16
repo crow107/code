59a62a8ad7aa513399b19bf8268616bec6d932db
Kanishk <rawat27kanishk@gmail.com>
2022-02-16 22:51:23 +0530

    commit

---
+++ html/projects/MusicPlayer(project2)/musicplayer.js
@@ -0,0 +1,31 @@
+
+function yo(){
+    var elem2 = document.getElementById("span2")
+    if (elem2.style.display =='block'){
+        
+        let elem = document.getElementById("span1")
+        
+        elem.style.transform = "rotate(45deg) translate(4px, 10px)"
+        
+
+        elem2.style.display = "none";
+        
+        let elem3 = document.getElementById("span3")
+        elem3.style.transform = "rotate(-45deg) translate(0px,-5px)"
+        var men = document.getElementById("menbar")
+        men.style.display= 'block';
+        
+}
+    else{
+        let elem = document.getElementById("span1")
+        elem.style.transform = "rotate(0) translate(0, 0)"
+
+        let elem2 = document.getElementById("span2")
+        elem2.style.display = "block";
+    
+        let elem3 = document.getElementById("span3")
+        elem3.style.transform = "rotate(0) translate(0)"
+        var men = document.getElementById("menbar")
+        men.style.display = 'none';
+    }
+}
